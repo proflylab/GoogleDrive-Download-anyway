f5c9976917ce404c1675731c2760a1d9f86ee09e
Proflylab <proflylab@protonmail.com>
2018-02-23 00:35:01 +0700

    0.0.2

---
+++ google.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         GoogleDrive Download anyway
+// @namespace    https://konayuki.moe/
+// @version      0.1
+// @description  Skip GoogleDrive can't scan this file for viruses
+// @author       Proflylab
+// @icon         https://avatars1.githubusercontent.com/u/27909158?s=460&v=4
+// @match        https://drive.google.com/uc?id=*
+// @match        https://docs.google.com/uc?id=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    document.getElementById("uc-download-link").click();
+    document.title = "GoogleDrive Download anyway";
+    document.body.innerHTML = document.readyState;//"Skip GoogleDrive can't scan this file for viruses";
+})();
